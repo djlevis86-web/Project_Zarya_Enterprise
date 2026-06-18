3d1af5b43659af83d66065488c1889c3fb25e8a4
Levis <Bylevinskiy@yandex.ru>
2026-06-18 15:52:23 +0300

    Add audit log interface

--- audit/views.py
+++ audit/views.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import render

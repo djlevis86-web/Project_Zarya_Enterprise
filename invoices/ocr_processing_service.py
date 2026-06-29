a1e57b17ed6492ae06dea859a5dabeee74287173
Levis <Bylevinskiy@yandex.ru>
2026-06-30 01:30:20 +0300

    Centralize OCR invoice identity application

--- invoices/ocr_processing_service.py
+++ invoices/ocr_processing_service.py
@@ -32,6 +32,72 @@ def read_and_parse_invoice_file(file_path):
     return text, parsed
 
 
+
+def apply_ocr_identity_to_invoice(invoice, parsed):
+    """
+    Применяет к счету номер, дату и поставщика из OCR.
+
+    Если OCR нашел номер и дату, но такой активный счет уже есть,
+    номер текущего счета не меняем и возвращаем предупреждение.
+    """
+
+    parsed_invoice_number = parsed.get(
+        'invoice_number'
+    )
+
+    parsed_invoice_date = parsed.get(
+        'invoice_date'
+    )
+
+    number_warning = ''
+
+    if parsed_invoice_number and parsed_invoice_date:
+
+        duplicate_invoice = (
+            Invoice.objects
+            .filter(
+                invoice_number=parsed_invoice_number,
+                invoice_date=parsed_invoice_date,
+            )
+            .exclude(
+                id=invoice.id
+            )
+            .exclude(
+                status=Invoice.STATUS_REJECTED
+            )
+            .first()
+        )
+
+        if duplicate_invoice:
+
+            number_warning = (
+                f'OCR нашел номер {parsed_invoice_number} '
+                f'от {parsed_invoice_date}, '
+                f'но такой счет уже есть: #{duplicate_invoice.id}. '
+                'Номер текущего счета не изменен.'
+            )
+
+        else:
+
+            invoice.invoice_number = parsed_invoice_number
+
+    elif parsed_invoice_number:
+
+        invoice.invoice_number = parsed_invoice_number
+
+    else:
+
+        invoice.invoice_number = None
+
+    invoice.invoice_date = parsed_invoice_date
+
+    invoice.vendor = parsed.get(
+        'vendor'
+    )
+
+    return number_warning
+
+
 def run_invoice_ocr_processing(invoice, user, log_action):
 
     if not invoice.file:
@@ -68,58 +134,9 @@ def run_invoice_ocr_processing(invoice, user, log_action):
 
         invoice.ocr_text = text
 
-        parsed_invoice_number = parsed.get(
-            'invoice_number'
-        )
-
-        parsed_invoice_date = parsed.get(
-            'invoice_date'
-        )
-
-        number_warning = ''
-
-        if parsed_invoice_number and parsed_invoice_date:
-
-            duplicate_invoice = (
-                Invoice.objects
-                .filter(
-                    invoice_number=parsed_invoice_number,
-                    invoice_date=parsed_invoice_date,
-                )
-                .exclude(
-                    id=invoice.id
-                )
-                .exclude(
-                    status=Invoice.STATUS_REJECTED
-                )
-                .first()
-            )
-
-            if duplicate_invoice:
-
-                number_warning = (
-                    f'OCR нашел номер {parsed_invoice_number} '
-                    f'от {parsed_invoice_date}, '
-                    f'но такой счет уже есть: #{duplicate_invoice.id}. '
-                    'Номер текущего счета не изменен.'
-                )
-
-            else:
-
-                invoice.invoice_number = parsed_invoice_number
-
-        elif parsed_invoice_number:
-
-            invoice.invoice_number = parsed_invoice_number
-
-        else:
-
-            invoice.invoice_number = None
-
-        invoice.invoice_date = parsed_invoice_date
-
-        invoice.vendor = parsed.get(
-            'vendor'
+        number_warning = apply_ocr_identity_to_invoice(
+            invoice,
+            parsed
         )
 
         amount_warning = apply_ocr_amount_to_invoice(

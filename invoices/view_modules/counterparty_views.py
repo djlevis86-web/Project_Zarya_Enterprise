c22d4ab961a5017ff1a85e393aaa2d6e14408b61
Levis <Bylevinskiy@yandex.ru>
2026-06-30 11:24:22 +0300

    Clean counterparty views imports

--- invoices/view_modules/counterparty_views.py
+++ invoices/view_modules/counterparty_views.py
@@ -1,102 +1,11 @@
-from django.utils.dateparse import parse_date
-from datetime import date, datetime, timedelta
-from decimal import Decimal
-from pathlib import Path
-import hashlib
-import traceback
-import uuid
-from django.conf import settings
 from django.contrib import messages
 from django.contrib.admin.views.decorators import staff_member_required
-from django.contrib.auth import get_user_model
-from django.contrib.auth.decorators import login_required
-from django.core.exceptions import PermissionDenied
-from django.core.files.storage import FileSystemStorage
 from django.core.paginator import Paginator
 from django.db.models import Count, Q, Sum
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
-from openpyxl import Workbook
-from openpyxl.styles import Alignment, Font, PatternFill
-from ..comment_forms import InvoiceCommentForm
-from ..comment_models import InvoiceComment
-from ..counterparty_service import (
-    extract_requisites_near_vendor,
-    normalize_counterparty_name,
-)
-from ..forms import (
-    CounterpartyImportForm,
-    CounterpartyManualForm,
-    InvoiceCounterpartyAssignForm,
-    InvoiceEditForm,
-    InvoiceForm,
-)
-from ..log_service import create_invoice_log
-from ..models import (
-    CompanyRequisites,
-    Counterparty,
-    Invoice,
-    InvoiceUploadBatch,
-    OCRJob,
-)
-from ..one_c_import_service import import_counterparties_from_file
-from ..ocr_processing_service import (
-    apply_ocr_identity_to_invoice,
-    get_duplicate_invoice_by_ocr_identity,
-    read_and_parse_invoice_file,
-    run_invoice_ocr_processing,
-)
-from ..ocr_verification_service import (
-    apply_ocr_amount_to_invoice,
-    sync_invoice_amount_verification,
-)
-from ..payment_registry_services import get_active_registry_items_for_invoice
-from audit.models import AuditLog
-from audit.services import log_action
-from ..payment_registry_permissions import (
-    require_payment_registry_permission,
-    user_can_cancel_payment_registry,
-    user_can_check_payment_registry,
-    user_can_export_payment_registry,
-    user_can_manage_payment_registry,
-    user_can_mark_payment_registry_paid,
-)
-
-from django.contrib import messages
-from django.contrib.admin.views.decorators import staff_member_required
-from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator
-from django.db.models import Q, Sum
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, redirect, render
-
-from openpyxl import Workbook
-from openpyxl.styles import Alignment, Font, PatternFill
-from openpyxl.utils import get_column_letter
-
 from ..forms import CounterpartyManualForm
 from ..models import Counterparty, Invoice
 
-
-
-
-from .counterparty_unmatched_views import (
-    export_unmatched_counterparties_excel,
-    unmatched_counterparties,
-)
-
-
-
-
-from .counterparty_import_views import (
-    counterparties_missing_requisites,
-    import_counterparties_1c,
-    rematch_counterparties_1c,
-)
-
-from .counterparty_assignment_views import (
-    invoice_assign_counterparty,
-)
 
 @staff_member_required
 def counterparty_directory(request):
